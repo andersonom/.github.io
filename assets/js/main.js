6d24e68fbcdf59030e953b6508748d9f616325e3
Anderson Martins <andersonom@gmail.com>
2019-03-16 17:45:25 -0300

    Refactor

--- assets/js/main.js
+++ assets/js/main.js
@@ -59,12 +59,9 @@ jQuery(document).ready(function($) {
         }
     );
     
-    /* Github Calendar - https://github.com/IonicaBizau/github-calendar */
-    GitHubCalendar("#github-graph", "IonicaBizau");
-    
+    /* Github Calendar - https://github.com/IonicaBizau/github-calendar */    
+    GitHubCalendar("#github-graph", "andersonom", { responsive: true });    
     
     /* Github Activity Feed - https://github.com/caseyscarborough/github-activity */
-    GitHubActivity.feed({ username: "caseyscarborough", selector: "#ghfeed" });
-
-
+    GitHubActivity.feed({ username: "andersonom", selector: "#ghfeed" });
 });
